a5f7816fce30998cfdffeff9220cf54b82f6450b
Matej Cerman <matej_cerman@college.harvard.edu>
2020-04-25 23:21:44 +0200

    Functioning version of all visualizations

--- web/app.R
+++ web/app.R
@@ -2,6 +2,7 @@ library(shiny)
 library(shinythemes)
 library(sf)
 library(leaflet)
+library(broom)
 
 source('gather.R')
 source('helpers.R')
@@ -29,38 +30,44 @@ schools_ec <- schools_ec %>%
         mat_aj = rowMeans(
             select(., mat_ajb1, mat_ajb2, mat_ajc1),
             na.rm = T
-        )
-    )
+        ),
+        avg_wage = 12*avg_wage/1000,
+        pop_total = pop_total/1000,
+        log_dens = log(pop_density)
+    ) %>%
+    rename(avg_income = avg_wage)
 
 levels(schools_ec$region)[3] <- 'Trenčiansky'
+levels(schools_ec$type)[c(2,3,4)] <- c('Primary', 'College preparatory', 'Vocational')
 
 pr <- schools_ec %>%
-    filter(type == 'Základná škola')
+    filter(type == 'Primary')
 
 hs <- schools_ec %>%
-    filter(type == 'Stredná odborná škola' | type == 'Gymnázium')
+    filter(type == 'Vocational' | type == 'College preparatory')
 
 
 regional_pr <- pr %>%
     group_by(region, county) %>%
     summarise(
-        overall = mean(overall_rating, na.rm = T),
+        overall_rating = mean(overall_rating, na.rm = T),
         testovanie9 = mean(testovanie9, na.rm = T),
         t9_sj = mean(t9_sj, na.rm = T),
         t9_m = mean(t9_m, na.rm = T),
         t9_mj = mean(t9_mj, na.rm = T),
         t9_s_ja_sl = mean(t9_s_ja_sl, na.rm = T),
-        pop = mean(pop_total),
-        dens = mean(pop_density),
-        log_dens = log(dens),
-        income = mean(avg_wage),
-        unempl = mean(unemployment_rate)
+        teachers = mean(teachers, na.rm = T),
+        pop_total = mean(pop_total),
+        pop_density = mean(pop_density),
+        log_dens = mean(log_dens),
+        avg_income = mean(avg_income),
+        unemployment_rate = mean(unemployment_rate)
     )
     
 regional_hs <- hs %>%
     group_by(region, county) %>%
     summarise(
-        overall = mean(overall_rating, na.rm = T),
+        overall_rating = mean(overall_rating, na.rm = T),
         maturity = mean(maturity, na.rm = T),
         mat_sj = mean(mat_sj, na.rm = T),
         mat_m = mean(mat_m, na.rm = T),
@@ -70,11 +77,12 @@ regional_hs <- hs %>%
         mat_ajb1 = mean(mat_ajb1, na.rm = T),
         mat_ajb2 = mean(mat_ajb2, na.rm = T),
         mat_ajc1 = mean(mat_ajc1, na.rm = T),
-        pop = mean(pop_total),
-        dens = mean(pop_density),
-        log_dens = log(dens),
-        income = mean(avg_wage),
-        unempl = mean(unemployment_rate),
+        teachers = mean(teachers, na.rm = T),
+        pop_total = mean(pop_total),
+        pop_density = mean(pop_density),
+        log_dens = mean(log_dens),
+        avg_income = mean(avg_income),
+        unemployment_rate = mean(unemployment_rate)
     )
 
 # Create datasets for mapping
@@ -110,25 +118,18 @@ geo_hs <- create_mapping_data(regional_hs)
 # Shiny web application UI
 
 ui <- navbarPage("Performance of Primary and Secondary Schools in Slovakia",
-                 
-    tabPanel('Overview'),
-    
     tabPanel('Regional Inequalities',
-             
         titlePanel('Regional Disparities in Demographic Indicators'),
-             
         sidebarLayout(
-            
             sidebarPanel(
                 selectInput('indicator1', "Select a statistic to display",
-                            choices = c('Unemployment rate (%)' = 'unempl',
-                                        'Average monthly salary (€)' = 'income',
+                            choices = c('Unemployment rate (%)' = 'unemployment_rate',
+                                        'Average gross income (in € thousands)' = 'avg_income',
                                         'Population density (logged)' = 'log_dens',
-                                        'Total population' = 'pop'),
+                                        'Total population (in 1000s)' = 'pop_total'),
                             selected = 'unempl'
                             )
                 ),
-            
             mainPanel(
                 leafletOutput('rin_map')
             )
@@ -165,24 +166,98 @@ ui <- navbarPage("Performance of Primary and Secondary Schools in Slovakia",
                     selectInput('indicator3', 'Select performance indicator',
                                 choices = school_gen_indicators,
                                 selected = 'overall_rating'),
-                    checkboxGroupInput('reg1', 'Select regions',
-                                       choices = c('All of Slovakia' = "svk", levels(schools_ec$region))),
+                    selectInput('reg1', 'Overall or regional data',
+                                choices = c('All Slovakia' = 'svk', 'Select regions' = 'regs'),
+                                selected = 'svk'),
+                    conditionalPanel(
+                        condition = "input.reg1 == 'regs'",
+                        checkboxGroupInput('reg2', 'Select regions',
+                                           choices = levels(schools_ec$region))),
                     conditionalPanel(
                         condition = "input.reg1 == 'svk'",
                         selectInput('colind1', 'Color by',
-                                    choices = c('none', 'lol'))
+                                    choices = c('None' = 'no',
+                                                'Major region' = 'super_region',
+                                                'Administrative region' = 'region',
+                                                'School type' = 'type',
+                                                'Public or private' = 'pub_pri'),
+                                    selected = 'no')
                     )
                 ),
                 mainPanel(
-                    
+                    plotOutput('schplot')
                 )
             )
         )
     ),
     
-    tabPanel('Model'),
+    tabPanel('Model',
+        headerPanel('Visualize simple linear models'),
+        fluidRow(
+            column(3,
+                   selectInput('level3', 'Select school level',
+                               choices = c('Primary schools (grade 1-9)' = 'pr',
+                                           'Secondary schools (grade 10-13)' = 'hs'),
+                               selected = 'pr'),
+                   selectInput('scope1', 'County averages or individual schools',
+                               choices = c('Counties' = 'county', 'Schools'= 'schools')),
+                   selectInput('resp1', 'Select outcome variable',
+                               choices = school_model_indicators,
+                               selected = 'overall_rating'),
+                   radioButtons('expl1', 'Select explanatory variable',
+                               choices = c('Average gross income (in € thousands)' = 'avg_income',
+                                           'County unemployment rate (%)' = 'unemployment_rate',
+                                           'Logged county population density (per km^2)' = 'log_dens',
+                                           'Total population (in 1000s)' = 'pop_total',
+                                           'Teachers per 100 students' = 'teachers'),
+                               selected = 'avg_income')
+                ),
+            column(6,
+                plotOutput('modplot1')
+            ),
+            column(3,
+                tableOutput('tab1')
+            )
+        )
+    ),
     
-    tabPanel('About')
+    tabPanel('About',
+        h1('Regional Inequality in Slovak Education', align = "center"),
+        br(),
+        fluidRow(column(2), column(8,
+            h2('Context'),
+                                   
+            "Slovakia (a Central European country) operates on 13-year pre-college educational
+            system which is typically split into a 9-year long primary school and 4 years of high
+            school. In 9th grade, students choose whether to apply to college-preparatory high schools
+            that teach a general curriculum or vocational schools which provide practical education for
+            a specific job (students from these schools typically don't continue to college).
+            Students take two national standardized tests in this time: a native language
+            and math test in 9th grade and a leaving examination in the final year of high school.
+            Both of these tests report raw percentages as their final outcome, wihtout any curving
+            or scoring formulas.",
+            
+            "There are significant regional disparities between the 79 counties (organized into 8 regions)
+            that consitute Slovakia. Cities tend to be significantly wealthier than rural areas,
+            and unemployment rates in southeastern and eastern Slovakia remain far above the national average.",
+            
+            h2('About this project'),
+            
+            'This project attempts to visualize, map, and find relationships between the performance,
+            of schools in various metrics and the socioeconomic conditions that their students and staff
+            live in. Importantly, it does not try to uncover causality either way, since the evaluated
+            data does not come from a controlled experiment, and in some cases contains limited sample
+            sizes. Both directions of causality (and a myriad of potential confounding variables) 
+            can be plausibly imagined: students from more advantaged backgrounds might have more resources
+            to succeed academically, but better schools might also contribute to an economic upliftment
+            in their region.',
+            
+            h2('Data'),
+            
+            'The project relies on [aje](http://skoly.sme.sk/metodika/#data) data from INEKO'
+        )
+        )
+    )
 )
 
 server <- function(input, output, session) {
@@ -280,15 +355,131 @@ server <- function(input, output, session) {
         }
     })
         
-       schin2 <- reactive({
+    schin2 <- reactive({
             if(input$level2 == 'pr') school_gen_indicators[9:15]
             else school_gen_indicators[1:8]
         })
+       
+    output$schplot <- renderPlot({
+        if (input$level2 == 'pr'){
+            if(input$reg1 == 'svk') {
+                if(input$colind1 == 'no') {
+                    pr %>%
+                        ggplot(aes_string(x = input$indicator3)) +
+                        geom_histogram()
+                }
+                    else if(!input$colind1 == 'no') {
+                        pr %>%
+                            ggplot(aes_string(x = input$indicator3, fill = input$colind1)) +
+                            geom_histogram()
+                    }
+            }
+            else if(input$reg1 == 'regs') {
+                req(input$reg2)
+                pr %>%
+                    filter(region %in% input$reg2) %>%
+                    ggplot(aes_string(x = input$indicator3)) +
+                    geom_histogram(aes(fill = region)) +
+                    facet_wrap(~region)
+                }
+            }
+                    
+        else if(input$level2 == 'hs') {
+            if(input$reg1 == 'svk') {
+                if(input$colind1 == 'no') {
+                    hs %>%
+                        ggplot(aes_string(x = input$indicator3)) +
+                        geom_histogram()
+                }
+                else if(!input$colind1 == 'no') {
+                    hs %>%
+                        ggplot(aes_string(x = input$indicator3, fill = input$colind1)) +
+                        geom_histogram()
+                }
+            }
+            else if(input$reg1 == 'regs') {
+                req(input$reg2)
+                hs %>%
+                    filter(region %in% input$reg2) %>%
+                    ggplot(aes_string(x = input$indicator3)) +
+                    geom_histogram(aes(fill = region)) +
+                    facet_wrap(~region)
+            }
+        }
+    })
+    
+    schin3 <- reactive({
+        if(input$level3 == 'pr') school_model_indicators[8:13]
+        else school_gen_indicators[1:7]
+    })
+    
+    output$modplot1 <- renderPlot({
+        if (input$level3 == 'pr') {
+            if (input$scope1 == 'county') {
+                regional_pr %>%
+                    ggplot(aes_string(input$expl1, input$resp1)) +
+                    geom_point(aes(color = region), alpha = 0.8, size = 3) +
+                    geom_smooth(method = 'lm', se = F)
+            }
+            else {
+                pr %>%
+                    ggplot(aes_string(input$expl1, input$resp1)) +
+                    geom_point(aes(color = region), alpha = 0.6, size = 1.5) +
+                    geom_smooth(method = 'lm', se = F)
+            }
+        }
+        else {
+            if (input$scope1 == 'county') {
+                regional_hs %>%
+                    ggplot(aes_string(input$expl1, input$resp1)) +
+                    geom_point(aes(color = region), alpha = 0.8, size = 3) +
+                    geom_smooth(method = 'lm', se = F)
+            }
+            else {
+                hs %>%
+                    ggplot(aes_string(input$expl1, input$resp1)) +
+                    geom_point(aes(color = region), alpha = 0.6, size = 1.5) +
+                    geom_smooth(method = 'lm', se = F)
+            }
+        }
+    })
+    
+    output$tab1 <- renderTable({
         
+        if (input$level3 == 'pr') {
+            if (input$scope1 == 'county') {
+                regional_pr %>%
+                    lm(as.formula(paste(input$resp1, '~', input$expl1)), data = .) %>%
+                    tidy(conf.int = T) %>%
+                    select(term, estimate, std.error)
+            }
+            else {
+                pr %>%
+                    lm(as.formula(paste(input$resp1, '~', input$expl1)), data = .) %>%
+                    tidy(conf.int = T) %>%
+                    select(term, estimate, std.error)
+            }
+        }
+        else {
+            if (input$scope1 == 'county') {
+                regional_hs %>%
+                    lm(as.formula(paste(input$resp1, '~', input$expl1)), data = .) %>%
+                    tidy(conf.int = T) %>%
+                    select(term, estimate, std.error)
+            }
+            else {
+                hs %>%
+                    lm(as.formula(paste(input$resp1, '~', input$expl1)), data = .) %>%
+                    tidy(conf.int = T) %>%
+                    select(term, estimate, std.error)
+            }
+        }
+    })
     
     observe({
         updateRadioButtons(session, 'indicator2', choices = schin1())
         updateSelectInput(session, 'indicator3', choices = schin2())
+        updateSelectInput(session, 'resp1', choices = schin3())
     })
     
 }
--- web/helpers.R
+++ web/helpers.R
@@ -1,19 +1,19 @@
 # Define a list of allowed school levels and visualization indicators
 
-reg_indicators <- c('Average monthly salary (gross)' = 'income',
-                    'Unemployment rate' = 'unempl',
-                    'Population density' = 'dens',
-                    'Total population' = 'pop')
+reg_indicators <- c('Average gross income (in € thousands)' = 'avg_income',
+                    'Unemployment rate (%)' = 'unemployment_rate',
+                    'Population density (per km^2)' = 'pop_density',
+                    'Total population (in 1000s)' = 'pop_total')
 
 school_map_indicators <- c(
-    'Overall rating (0-10 scale)' = 'overall',
+    'Overall rating (0-10 scale)' = 'overall_rating',
     'Leaving examination preformance (0-10 scale)' = 'maturity',
     'Average leaving exam score: Slovak language (%)' = 'mat_sj',
     'Average leaving exam score: Math (%)' = 'mat_m',
     'Average leaving exam score: English language (%)' = 'mat_aj',
     'Average leaving exam score: Hungarian language  (%)' = 'mat_mj',
     'Average leaving exam score: Slovak as second language (%)' = 'mat_s_ja_sl',
-    'Overall rating (0-10 scale)' = 'overall',
+    'Overall rating (0-10 scale)' = 'overall_rating',
     'Grade 9 standardized testing performance (0-10 scale)' = 'testovanie9',
     'Grade 9 test score: Slovak Language (%)' = 't9_sj',
     'Grade 9 test score: Math (%)' = 't9_m',
@@ -37,4 +37,20 @@ school_gen_indicators <- c(
   'Grade 9 test score: Hungarian Language (%)' = 't9_mj',
   'Grade 9 test score: Slovak as second language (%)' = 't9_s_ja_sl',
   'Teachers per 100 students' = 'teachers'
+)
+
+school_model_indicators <- c(
+  'Overall rating (0-10 scale)' = 'overall_rating',
+  'Leaving examination preformance (0-10 scale)' = 'maturity',
+  'Average leaving exam score: Slovak language (%)' = 'mat_sj',
+  'Average leaving exam score: Math (%)' = 'mat_m',
+  'Average leaving exam score: English language (%)' = 'mat_aj',
+  'Average leaving exam score: Hungarian language  (%)' = 'mat_mj',
+  'Average leaving exam score: Slovak as second language (%)' = 'mat_s_ja_sl',
+  'Overall rating (0-10 scale)' = 'overall_rating',
+  'Grade 9 standardized testing performance (0-10 scale)' = 'testovanie9',
+  'Grade 9 test score: Slovak Language (%)' = 't9_sj',
+  'Grade 9 test score: Math (%)' = 't9_m',
+  'Grade 9 test score: Hungarian Language (%)' = 't9_mj',
+  'Grade 9 test score: Slovak as second language (%)' = 't9_s_ja_sl'
 )
